d67ca2fff6faf4d55ef08b13fbad121dbaaae119
seohyemin99 <99458927+seohyemin99@users.noreply.github.com>
2022-02-27 01:50:28 +0900

    [Bronze IV] Title: 시험 성적, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/9498.시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+A = int(input())
+if A <= 100 and A >= 90 :
+    print("A")
+elif A <= 89 and A >= 80 :
+    print("B")
+elif A <= 79 and A >= 70:
+    print("C")
+elif A <= 69 and A >= 60:
+    print("D")
+else :
+    print("F")
