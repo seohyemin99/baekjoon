5cf33c15d5b26d6b2bade9853446a82ed5650cde
seohyemin99 <99458927+seohyemin99@users.noreply.github.com>
2022-03-29 13:47:16 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,16 +1,11 @@
 A, B = input().split()
-A = list(str(A))
-B = list(str(B))
-a, b = "", ""
-for i in range(0, len(A)) :
-    a += A[len(A)-i-1]
-for j in range(len(B)) :
-    b += B[len(B)-j-1]
-    
-if int(a) > int(b) :
-    print(a)
-elif int(b) > int(a): 
-    print(b)
+A = int(A[::-1])
+B = int(B[::-1])
+
+if A > B :
+    print(A)
+elif B > A: 
+    print(B)
 
 
     
